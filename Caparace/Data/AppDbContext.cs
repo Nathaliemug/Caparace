8760d7b490b47600708170ae88a975d51bcb1fe8
Nathaliemug <42577410+Nathaliemug@users.noreply.github.com>
2024-08-23 00:49:12 +0200

    Add project files.

---
+++ Caparace/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Caparace.Models;
+
+namespace Caparace.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Caparace.Models.Product> Product { get; set; } = default!;
+        public DbSet<Caparace.Models.Client> Client { get; set; } = default!;
+    }
+}
